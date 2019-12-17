fad93954cd55683778ac98a91f86518e6b8ea2f2
Josh Gribbon <josh.p.grib@gmail.com>
2019-12-17 03:45:52 -0500

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,98 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <!-- <img alt="Vue logo" style="height: 40px" src="./assets/bb-stats.jpg"> -->
-        <h1>
+  <v-app id="app">
+    <v-app-bar
+      app
+      dense
+      clipped-left
+      color="primary"
+    >
+      <v-toolbar-title>
+        <span class="headline font-weight-thin">
           uptime-monitor
-          <small class="text-muted">collect uptime info from different URLsI</small>
-        </h1>
-      </a>
-    </nav>
-    <section class="container-fluid">
-      <div class="row wrap">
-        <article
-          v-for="({ route, requests, timing }, idx) in routeStats"
-          :key="idx"
-          class="col-sm-3"
-        >
-          <h2>{{ route }}</h2>
-          <h3>Requests</h3>
-          <p>Total: {{ requests.total }}</p>
-          <p>Failed: {{ requests.failed }}</p>
-          <p>Succeeded: {{ requests.success }}</p>
-          <h3>Timing</h3>
-          <p>Total: {{ timing.samples }}</p>
-          <p>Total wait time: {{ timing.totalWaitTime }} ms</p>
-          <p>Average wait time: {{ timing.average }} ms</p>
-          <p>{{ timing.times }}</p>
-        </article>
-      </div>
-    </section>
-    <hr>
-    {{ history }}
-  </div>
+        </span>
+        <span class="subtitle-1 font-weight-bold font-italic">
+          collect uptime info from different URLs
+        </span>
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row>
+          <v-col
+            v-for="({ route, requests, timing }, idx) in routeStats"
+            :key="idx"
+          >
+            <v-card>
+              <v-card-title class="headline mb-1">
+                {{ route }}
+              </v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <v-sparkline
+                      :value="timing.times"
+                      auto-draw
+                      smooth
+                      gradientDirection="top"
+                      :gradient="['red', 'yellow', 'green']"
+                      height="50"
+                    />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <h3>Requests</h3>
+                    <p>Total: {{ requests.total }}</p>
+                    <p>Failed: {{ requests.failed }}</p>
+                    <p>Succeeded: {{ requests.success }}</p>
+                  </v-col>
+                  <v-col>
+                    <h3>Timing</h3>
+                    <p>Total: {{ timing.samples }}</p>
+                    <p>Total wait time: {{ timing.totalWaitTime }} ms</p>
+                    <p>Average wait time: {{ timing.average }} ms</p>
+                    <p>{{ timing.times }}</p>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  color="error"
+                  @click="deleteRoute(route)"
+                >
+                  <v-icon left>mdi-delete</v-icon>
+                  Delete
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <h2>History</h2>
+          {{ history }}
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 import axios from 'axios'
 
+const API_BASE = 'http://localhost:3000'
+
 export default {
   name: 'app',
   data () {
     return {
+      drawer: false,
       history: []
     }
   },
@@ -74,8 +126,11 @@ export default {
   },
   methods: {
     async loadHistory () {
-      const response = await axios.get('http://localhost:3000/history')
+      const response = await axios.get(`${API_BASE}/history`)
       this.history = response.data
+    },
+    async deleteRoute (route) {
+      return await axios.get(`${API_BASE}/routes/remove`, { route })
     }
   }
 }
