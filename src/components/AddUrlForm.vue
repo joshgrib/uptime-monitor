01854fd347bb5ee16089ec14cd3b0a8713066f84
Josh Gribbon <josh.p.grib@gmail.com>
2019-12-18 20:01:04 -0500

    Add more tailwind and refactor

---
+++ src/components/AddUrlForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="leading-tight mb-3 rounded-full shadow-lg">
+    <input
+      v-model="newUrlText"
+      type="text"
+      placeholder="http://www.my-url.to/ping"
+      class="p-3 w-4/5 bg-gray-100 text-gray-700 rounded-l-full"
+    >
+    <button
+      type="button"
+      class="p-3 w-1/5 text-white font-bold bg-blue-500 hover:bg-blue-700 rounded-r-full"
+      @click="submit"
+    >
+      Add (<kbd>Enter</kbd>)
+    </button>
+  </form>
+</template>
+<script>
+export default {
+  name: 'add-url-form',
+  data () {
+    return {
+      newUrlText: undefined
+    }
+  },
+  methods: {
+    submit () {
+      this.$emit('submit', this.newUrlText)
+      this.newUrlText = undefined
+    }
+  }
+}
+</script>
---
+++ src/components/MonitorItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <article
+    class="bg-gray-200 rounded-lg shadow-inner my-2 p-2"
+  >
+    <header class="text-lg">
+      {{ url }}
+    </header>
+    <p>{{ url }}</p>
+  </article>
+</template>
+<script>
+export default {
+  name: 'monitor-item',
+  props: {
+    url: {
+      type: String,
+      default () { return 'http://www.example.com' }
+    }
+  }
+}
+</script>
